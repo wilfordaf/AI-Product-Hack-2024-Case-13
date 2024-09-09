8bfde87d560f2fea5af70a05b208874ebd67d8c5
Sergey Yurpalov <79761555+wilfordaf@users.noreply.github.com>
2024-09-09 21:21:29 +0300

    feat: success endpoint type

--- src/service/custom_typing/endpoint_types/__init__.py
+++ src/service/custom_typing/endpoint_types/__init__.py
@@ -1,4 +1,5 @@
 from .generic_response_type import TGenericResponse
 from .ping_response_body_type import TPingResponseBody
+from .success_response_body_type import TSuccessResponseBody
 
-__all__ = ["TGenericResponse", "TPingResponseBody"]
+__all__ = ["TGenericResponse", "TPingResponseBody", "TSuccessResponseBody"]
