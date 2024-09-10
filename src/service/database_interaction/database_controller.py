8db1d552ce92616fff3bf4fed6b85fae2eedf0b5
Sergey Yurpalov <79761555+wilfordaf@users.noreply.github.com>
2024-09-10 21:47:22 +0300

    refactor: correct implementation of repo methods

--- src/service/database_interaction/database_controller.py
+++ src/service/database_interaction/database_controller.py
@@ -4,18 +4,19 @@ from typing import Any, Callable, List
 from sqlalchemy import create_engine, make_url
 from sqlalchemy.orm import sessionmaker
 
-from service.database_interaction.dto.tag.tag_dto import TagDTO
-from service.database_interaction.dto.user import UserCreateDTO, UserUpdateTagsDTO
-from service.database_interaction.repositories import (
+from src.service.database_interaction.dto.tag.tag_dto import TagDTO
+from src.service.database_interaction.dto.user import UserCreateDTO, UserUpdateTagsDTO
+from src.service.database_interaction.dto.user.user_dto import UserDTO
+from src.service.database_interaction.orm_models.base import Base
+from src.service.database_interaction.repositories import (
     CategoryRepository,
     EventRepository,
     RankingRequestRepository,
     TagRepository,
     UserRepository,
 )
-from service.utils.exceptions import ServiceError
-from service.utils.logging import ConsoleLogger
-from src.service.database_interaction.orm_models.base import Base
+from src.service.utils.exceptions import ServiceError
+from src.service.utils.logging import ConsoleLogger
 
 
 class DatabaseController:
@@ -23,6 +24,7 @@ class DatabaseController:
 
     def __init__(self):
         raw_connection_string = os.getenv(self._CONNECTION_STRING_VAR_NAME, "")
+        self._logger = ConsoleLogger()
         try:
             connection_url = make_url(raw_connection_string)
             engine = create_engine(connection_url)
@@ -39,18 +41,35 @@ class DatabaseController:
         self._event_repository = EventRepository(self._session_maker)
         self._ranking_request_repository = RankingRequestRepository(self._session_maker)
 
-        self._logger = ConsoleLogger()
-
     def add_user(self, user: UserCreateDTO) -> None:
-        self._execute_repository_method(self._user_repository.create, {"user_create_dto": user})
+        self._execute_repository_method(self._user_repository.create, user_create_dto=user)
 
     def update_user_tags(self, user_update_tags_dto: UserUpdateTagsDTO) -> None:
-        self._execute_repository_method(self._user_repository, {"user_update_dto": user_update_tags_dto})
+        self._execute_repository_method(
+            self._user_repository.add_tags_to_user,
+            user_update_dto=user_update_tags_dto,
+        )
+
+    def is_admin(self, user_telegram_id: str, event_title: str) -> bool:
+        is_admin: bool = self._execute_repository_method(
+            self._event_repository.is_admin,
+            telegram_id=user_telegram_id,
+            event_title=event_title,
+        )
+        return is_admin
 
     def get_all_tags(self) -> List[TagDTO]:
-        return self._execute_repository_method(self._tag_repository.get_all_tags)
+        all_tags: List[TagDTO] = self._execute_repository_method(self._tag_repository.get_all_tags)
+        return all_tags
 
-    def _execute_repository_method(self, method: Callable[[Any], Any], **kwargs) -> Any:
+    def get_users_by_telegram_ids(self, telegram_ids: List[str]) -> List[UserDTO]:
+        users: List[UserDTO] = self._execute_repository_method(
+            self._user_repository.get_users_by_telegram_ids,
+            telegram_i=telegram_ids,
+        )
+        return users
+
+    def _execute_repository_method(self, method: Callable[..., Any], **kwargs) -> Any:
         try:
             result = method(**kwargs)
             self._logger.debug(f"{method.__name__} with {kwargs} executed successfully")
