69dd4c7cffbced619de2733c4e0aefaac1baafe0
Sergey Yurpalov <79761555+wilfordaf@users.noreply.github.com>
2024-09-10 21:46:48 +0300

    fix: sessionmaker

--- src/service/database_interaction/repositories/event_repository.py
+++ src/service/database_interaction/repositories/event_repository.py
@@ -1,6 +1,6 @@
 from typing import List, Optional
 
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, sessionmaker
 
 from src.service.database_interaction.dto.event import EventCreateDTO, EventDTO
 from src.service.database_interaction.orm_models.events import Events
@@ -8,53 +8,58 @@ from src.service.database_interaction.orm_models.users import Users
 
 
 class EventRepository:
-    def __init__(self, session: Session):
-        self._session = session
+    def __init__(self, session_maker: sessionmaker[Session]):
+        self._session_maker = session_maker
 
     def create(self, event_create_dto: EventCreateDTO, admin_id: int) -> None:
-        admin = self._session.query(Users).filter(Users.id == admin_id).first()
-        if not admin:
-            raise ValueError("Admin user not found")
+        with self._session_maker() as session:
+            admin = session.query(Users).filter(Users.id == admin_id).first()
+            if not admin:
+                raise ValueError("Admin user not found")
 
-        event = Events(
-            title=event_create_dto.title,
-            description=event_create_dto.description,
-            admin=admin,  # Set the admin relationship
-        )
-        self._session.add(event)
-        self._session.commit()
-        self._session.refresh(event)
+            event = Events(
+                title=event_create_dto.title,
+                description=event_create_dto.description,
+                admin=admin,  # Set the admin relationship
+            )
+            session.add(event)
+            session.commit()
+            session.refresh(event)
 
     def read_by_title(self, title: str) -> Optional[EventDTO]:
-        event = self._session.query(Events).filter(Events.title == title).first()
-        return EventDTO.model_validate(event) if event else None
+        with self._session_maker() as session:
+            event = session.query(Events).filter(Events.title == title).first()
+            return EventDTO.model_validate(event) if event else None
 
     def delete(self, event_id: int):
-        event = self._session.query(Events).filter(Events.id == event_id).first()
-        if not event:
-            return
+        with self._session_maker() as session:
+            event = session.query(Events).filter(Events.id == event_id).first()
+            if not event:
+                return
 
-        self._session.delete(event)
-        self._session.commit()
+            session.delete(event)
+            session.commit()
 
     def add_users_to_event(self, event_id: int, user_ids: List[int]) -> None:
-        event = self._session.query(Events).filter(Events.id == event_id).first()
-        if not event:
-            return
+        with self._session_maker() as session:
+            event = session.query(Events).filter(Events.id == event_id).first()
+            if not event:
+                return
 
-        users = self._session.query(Users).filter(Users.id.in_(user_ids)).all()
-        for user in users:
-            event.users.append(user)
+            users = session.query(Users).filter(Users.id.in_(user_ids)).all()
+            for user in users:
+                event.users.append(user)
 
-        self._session.commit()
+            session.commit()
 
     def is_admin(self, event_title: str, user_telegram_id: str) -> bool:
-        event = self.read_by_title(event_title)
-        if not event:
-            return False
+        with self._session_maker() as session:
+            event = self.read_by_title(event_title)
+            if not event:
+                return False
 
-        admin_user = self._session.query(Users).filter(Users.telegram_id == user_telegram_id).first()
-        if not admin_user:
-            return False
+            admin_user = session.query(Users).filter(Users.telegram_id == user_telegram_id).first()
+            if not admin_user:
+                return False
 
-        return event.admin_id == admin_user.id
+            return bool(event.admin_id == admin_user.id)
