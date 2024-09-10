3044a0b3a823ad51b1f36b3e2f4a9355e5216cc4
Sergey Yurpalov <79761555+wilfordaf@users.noreply.github.com>
2024-09-10 21:44:28 +0300

    feat: rm monitoring

--- src/service/utils/monitoring/__init__.py
+++
@@ -1,7 +0,0 @@
-from .monitoring_metrics import (
-    api_version,
-    cpu_utilization_percent,
-    log_levels_distribution,
-)
-
-__all__ = ["cpu_utilization_percent", "log_levels_distribution", "api_version"]
