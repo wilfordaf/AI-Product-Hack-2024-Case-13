6c71c005e53dc6bfc2337727c8431ba5110c63a3
khrstln <Khrstln@yandex.ru>
2024-09-10 13:58:30 +0300

    feat: update json parser

--- src/service/data_formatting/file_parsers/json_file_parser.py
+++ src/service/data_formatting/file_parsers/json_file_parser.py
@@ -2,17 +2,21 @@ import json
 from typing import Any, Dict
 
 from src.service.data_formatting.file_parsers.file_parser import FileParser
+from src.service.utils.exceptions import ServiceError
 
 
 class JsonFileParser(FileParser):
     _EXTENSION = ".json"
 
-    def _parse_file(self) -> str:
+    def retrieve_data(self, from_id: str) -> str:
+        try:
+            return self._parse_file(from_id)
+        except Exception as e:
+            raise ServiceError(f"При чтении файла {self._filepath} возникла ошибка: {str(e)}") from e
+
+    def _parse_file(self, from_id: str) -> str:
         with open(self._filepath, encoding="utf-8") as fin:
             content: Dict[str, Any] = json.load(fin)
 
-            text = ""
-            for message in content['messages']:
-                text += message['text'] + " "
+            return "\n".join([message for message in content['message'] if message['from_id'] == from_id])
 
-            return text
