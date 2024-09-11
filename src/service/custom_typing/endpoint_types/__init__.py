31ad726be5b4c08e6d5cd1edd3547b2315fd6003
Sergey Yurpalov <79761555+wilfordaf@users.noreply.github.com>
2024-09-11 09:50:15 +0300

    feat: tag titles endpoint type

--- src/service/custom_typing/endpoint_types/__init__.py
+++ src/service/custom_typing/endpoint_types/__init__.py
@@ -1,6 +1,13 @@
 from .generic_response_type import TGenericResponse
 from .ping_response_body_type import TPingResponseBody
 from .success_response_body_type import TSuccessResponseBody
+from .tag_titles_response_type import TTagTitlesResponse
 from .user_ranking_response_body_type import TUserRankingResponseBody
 
-__all__ = ["TGenericResponse", "TPingResponseBody", "TSuccessResponseBody", "TUserRankingResponseBody"]
+__all__ = [
+    "TGenericResponse",
+    "TPingResponseBody",
+    "TSuccessResponseBody",
+    "TUserRankingResponseBody",
+    "TTagTitlesResponse",
+]
