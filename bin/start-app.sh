f06ab206cf9765519ae27f9633bce1d1c464642c
Sergey Yurpalov <79761555+wilfordaf@users.noreply.github.com>
2024-09-11 12:15:26 +0300

    chore: start ollama on gpu, load target model during startup

--- bin/start-app.sh
+++ bin/start-app.sh
@@ -5,5 +5,10 @@ while ! nc -z db 5432; do
   sleep 1
 done
 
-echo "PostgreSQL is up - executing command"
+while ! nc -z ollama 11434; do
+  echo "Waiting for Ollama..."
+  sleep 1
+done
+
+echo "Satellite services are up - executing command"
 uvicorn src.entrypoint.api_ui:app --host 0.0.0.0 --port 9090
